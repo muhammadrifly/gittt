c2ae93835023c7af4f4a6a0693ea9f428fbf5c36
panjisob <sobaripanji8@gmail.com>
2020-08-08 10:44:31 +0700

    resolve

--- config/database.php
+++ config/database.php
@@ -33,6 +33,13 @@ return [
     |
     */
 
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
+
     'connections' => [
 
         'sqlite' => [
